9056a2078b90378533f223d94bc83ab61f0f1598
Christopher Danella <cdanella@yhmg.com>
2019-04-05 11:19:26 -0400

    Generates orders table and model, adds early model tests

---
+++ app/models/order.rb
@@ -0,0 +1,9 @@
+class Order < ApplicationRecord
+  ORDER_STATUS = ['pending', 'processing', 'fulfilled', 'delivered', 'canceled'] 
+  
+  belongs_to :customer
+  
+  validates_presence_of :customer
+  validates :status, inclusion: { in: ORDER_STATUS }
+   
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_215024) do
+ActiveRecord::Schema.define(version: 2019_04_05_150116) do
 
   create_table "collections", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2019_02_27_215024) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id"
+    t.float "total_cost"
+    t.string "status", default: "pending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "products", force: :cascade do |t|
